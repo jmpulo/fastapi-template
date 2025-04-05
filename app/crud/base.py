f84c34bb5267c93722b15bfed23b45f7488407c9
Chema <jmpujades.dev@proton.me>
2025-04-05 13:13:47 +0200

    ⚠️ fix deprecation warning on sqlmodel

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,7 +1,7 @@
 from typing import Generic, Type, TypeVar
 from uuid import UUID
 
-from sqlmodel import Session, SQLModel
+from sqlmodel import Session, SQLModel, select
 
 ModelType = TypeVar("ModelType", bound=SQLModel)
 CreateModelType = TypeVar("CreateModelType", bound=SQLModel)
@@ -16,7 +16,7 @@ class CRUDBase(Generic[ModelType, CreateModelType, UpdateModelType]):
         return db.get(self.model, id)
 
     def get_multi(self, db: Session) -> ModelType | None:
-        return db.query(self.model).all()
+        return db.exec(select(self.model))
 
     def create(self, db: Session, *, obj_in: CreateModelType) -> ModelType:
         db_obj = self.model.model_validate(obj_in)
