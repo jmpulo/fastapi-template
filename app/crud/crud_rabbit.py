91a9d2265ebd78bcdcc7e7ba9d82f5ea5b81e601
Chema <jmpujades.dev@proton.me>
2025-04-05 11:06:15 +0200

    refactoring

--- app/crud/crud_rabbit.py
+++ app/crud/crud_rabbit.py
@@ -1,4 +1,4 @@
-from app.crud.base import CRUDBase
+from .base import CRUDBase
 from app.models import Rabbit, RabbitCreate, RabbitUpdate
 
 
--- app/main.py
+++ app/main.py
@@ -6,17 +6,18 @@ from app.core.config import settings
 from contextlib import asynccontextmanager
 
 
-app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Lifespan event for the FastAPI app."""
     init_db()
     yield
 
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan,
+)
 
 app.add_middleware(
     CORSMiddleware,
