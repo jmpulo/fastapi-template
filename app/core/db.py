e494fc94c83e8cc3438d7cc88b6aef4ae44f0469
Chema <jmpujades.dev@proton.me>
2025-04-07 20:19:27 +0200

    💄 remove unused comments

--- app/core/db.py
+++ app/core/db.py
@@ -2,9 +2,11 @@ from sqlmodel import SQLModel, create_engine
 
 from .config import settings
 
-# connect_args = {"check_same_thread": False}
-# , connect_args=connect_args
-engine = create_engine(settings.DB_PATH, echo=True)
+
+engine = create_engine(
+    settings.DB_PATH,
+    echo=True,
+)
 
 
 def init_db():
